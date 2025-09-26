3577b7c842bd44ee3682ec32ef6c6d76b5b9c0f0
Sridda Joka <sridda@gmail.com>
2025-09-26 23:06:32 +0530

    resolved navbar z-index bug

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import { FiHome, FiFolder, FiMail, FiTool } from "react-icons/fi";
 function Navbar(){
     return(
         <>
-            <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 h-[48px] w-[292px] bg-[#1C1F21] mx-auto my-2 rounded-lg flex justify-center gap-12 items-center shadow-lg">
+            <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 h-[48px] w-[292px] bg-[#1C1F21] mx-auto my-2 rounded-lg flex justify-center gap-12 items-center shadow-lg  z-[9999]">
                 <div className="flex flex-col relative items-center cursor-pointer group">
                     <a href="#about"  onTouchEnd={(e)=>e.currentTarget.blur()} onFocus={(e) => e.currentTarget.blur()} ><FiHome className="text-2xl mb-1 text-[#E6B800] transition-transform duration-200 group-hover:scale-125 group-hover:drop-shadow-[0_0_10px_#E6B800] active:scale-125 active:drop-shadow-[0_0_10px_#E6B800]"/></a>
                     <span className="absolute top-10 text-s text-[#E6B800] opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap">About</span>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,23 +4,23 @@ import {motion} from "framer-motion"
 function ProjectCard({ title, demo, repo, desc }) {
     const [isHover,setIsHover]=useState(false);
     return (
-        <div className="w-68 h-60 perspective"
+        <div className="w-68 h-60 [perspective:1000px]"
             onMouseEnter={()=>setIsHover(true)}
             onMouseLeave={()=>setIsHover(false)}
         >
             
-            <motion.div className="relative w-full h-full preserve-3d cursor-pointer"
+            <motion.div className="relative w-full h-full [transform-style:preserve-3d] cursor-pointer"
             animate={{ rotateY: isHover ? 180 : 0 }}
             transition={{ duration: 0.6, ease: "easeInOut" }}
             >
             
-            <div className="absolute w-full h-full bg-[#1A1A1C] p-8 rounded-lg flex flex-col gap-4 items-center justify-center text-center text-white backface-hidden">
+            <div className="absolute w-full h-full bg-[#1A1A1C] p-8 rounded-lg flex flex-col gap-4 items-center justify-center text-center text-white [backface-visibility:hidden]">
                 <h6 className="text-[#FFD700] text-2xl sm:text-2xl md:text-2xl lg:text-2xl">{title}</h6>
                 <p>{desc}</p>
                 
             </div>
 
-            <div className="absolute w-full h-full bg-[#FFD700] p-8 rounded-lg flex flex-col gap-3 items-center justify-center text-center text-black backface-hidden rotate-y-180">
+            <div className="absolute w-full h-full bg-[#FFD700] p-8 rounded-lg flex flex-col gap-3 items-center justify-center text-center text-black [backface-visibility:hidden] [transform:rotateY(180deg)]">
                 {demo?<a href={demo} target="_blank" className="flex items-center gap-2 hover:underline"><FaExternalLinkAlt className="text-lg text-[#060606]" />Live Site</a>:null}
                 <a href={repo} target="_blank" className="flex items-center gap-2 hover:underline"><FaGithub className="text-lg text-[#060606]" />Repository</a>
             </div>
